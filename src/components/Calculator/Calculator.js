07cc15f807d783016c2c8951685f1d889b5c841e
haley-r <haleyryan77@gmail.com>
2020-11-02 11:01:15 -0600

    calculator input functions

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,4 +1,5 @@
 // imports
+import React, { useState } from 'react';
 // (redux? axios?)
 
 
@@ -9,34 +10,70 @@
 // a database (via redux/store?)
 
 function Calculator(props) {
+    // initialize the variables needed for making a calculation
+    const [firstNumber, setFirstNumber] = useState('');
+    const [operator, setOperator] = useState({});
+    const [secondNumber, setSecondNumber] = useState('');
+
+    const numberInput =(event, value)=> {
+        // if there is an operator set, add to second number string        
+        if (operator.type){
+            setSecondNumber(secondNumber + value);
+        }
+        // if theres not an operator set, add to the first number string
+        else{
+            setFirstNumber(firstNumber + value);
+        }
+    }
+
+    const operatorInput =(event, type, symbol)=> {
+        //if someone chooses an operator before choosing a first number
+        //use last answer from redux store
+        setOperator({
+            type: type,
+            symbol: symbol
+        })
+    }
+
+    const runCalculation=()=>{
+        console.log('so you wanna run the calculation huh');
+        //dispatch values somewhere
+    }
+    const clearAll=()=>{
+        setFirstNumber('');
+        setOperator({});
+        setSecondNumber('');
+    }
+
     return (
         <div>
             <h2>Calculator</h2>
-            <p>put calculator interface here</p>
+            <p>calculation display: {firstNumber} {operator.symbol} {secondNumber}</p>
             <div>
-                {/* could be a number button component */}
-                <button>0</button>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>1</button>
+                {/* could be a number button component? */}
+                <button onClick={(event) => numberInput(event, '0')}>0</button>
+                <button onClick={(event) => numberInput(event, '1')}>1</button>
+                <button onClick={(event) => numberInput(event, '2')}>2</button>
+                <button onClick={(event) => numberInput(event, '3')}>3</button>
+                <button onClick={(event) => numberInput(event, '4')}>4</button>
+                <button onClick={(event) => numberInput(event, '5')}>5</button>
+                <button onClick={(event) => numberInput(event, '6')}>6</button>
+                <button onClick={(event) => numberInput(event, '7')}>7</button>
+                <button onClick={(event) => numberInput(event, '8')}>8</button>
+                <button onClick={(event) => numberInput(event, '9')}>9</button>
             </div>
             <div>
                 {/* could be an operator button component */}
-                <button>+</button>
-                <button>-</button>
-                <button>*</button>
-                <button>/</button>
+                <button onClick={(event) => operatorInput(event, 'ADD', '+')}>+</button>
+                <button onClick={(event) => operatorInput(event, 'SUBTRACT', '-')}>-</button>
+                <button onClick={(event) => operatorInput(event, 'MULTIPY', '*')}>*</button>
+                <button onClick={(event) => operatorInput(event, 'DIVIDE', '/')}>/</button>
             </div>
             <div>
                 {/* could be a special calculate component */}
-                <button>=</button>
+                <button onClick={runCalculation}>=</button>
+                <button onClick={clearAll}>C</button>
+                {/* maybe also have backspace? if time */}
             </div>
 
         </div>
